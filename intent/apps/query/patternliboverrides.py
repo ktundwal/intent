e8a4b2d3fb25eef74ba7e56a399a3567c7c1c8d7
Kapil Tundwal <kapil@cruxly.com>
2012-07-28 08:55:36 -0600

    Overriding Twitter search API wrapper in pattern clips lib since it doesnt give us tweet id. We are only going to store tweet id in our database for PURL to conserve space. This assumes twitter can always provide the content for given tweet id, no matter how old tweet is.

---
+++ intent/apps/query/patternliboverrides.py
@@ -0,0 +1,71 @@
+__author__ = 'browsepad'
+
+from pattern.web import *
+
+
+#--- PATTERN CLIPS LIB TWITTER CLASS OVERRIDE -------------------------------------------------------------------------
+
+class TwitterEx(SearchEngine):
+
+    def __init__(self, license=None, throttle=0.5, language=None):
+        SearchEngine.__init__(self, license or TWITTER_LICENSE, throttle, language)
+
+    def search(self, query, type=SEARCH, start=1, count=10, sort=RELEVANCY, size=None, cached=False, **kwargs):
+        """ Returns a list of results from Twitter for the given query.
+            - type : SEARCH or TRENDS,
+            - start: maximum 1500 results (10 for trends) => start 1-15 with count=100, 1500/count,
+            - count: maximum 100, or 10 for trends.
+            There is an hourly limit of 150+ queries (actual amount undisclosed).
+        """
+        if type != SEARCH:
+            raise SearchEngineTypeError
+        if not query or count < 1 or start < 1 or start > 1500/count:
+            return Results(TWITTER, query, type)
+            # 1) Construct request URL.
+        url = URL(TWITTER + "search.json?", method=GET)
+        url.query = {
+            "q": query,
+            "page": start,
+            "rpp": min(count, 100)
+        }
+        if "geo" in kwargs:
+            # Filter by location with geo=(latitude, longitude, radius).
+            # It can also be a (latitude, longitude)-tuple with default radius "10km".
+            url.query["geocode"] = ",".join((map(str, kwargs.pop("geo")) + ["10km"])[:3])
+            # 2) Restrict language.
+        url.query["lang"] = self.language or ""
+        # 3) Parse JSON response.
+        kwargs.setdefault("unicode", True)
+        kwargs.setdefault("throttle", self.throttle)
+        try:
+            data = URL(url).download(cached=cached, **kwargs)
+        except HTTP420Error:
+            raise SearchEngineLimitError
+        data = json.loads(data)
+        results = Results(TWITTER, query, type)
+        results.total = None
+        for x in data.get("results", data.get("trends", [])):
+            r = Result(url=None)
+            try:
+                r.url         = self.format(TWITTER_SOURCE.search(x.get("source", "href=&quot;&quot;")).group(1))
+            except Exception, e:
+                r.url = ''
+            r.description = self.format(x.get("text"))
+            r.date        = self.format(x.get("created_at", data.get("as_of")))
+            r.author      = self.format(x.get("from_user"))
+            r.profile     = self.format(x.get("profile_image_url")) # Profile picture URL.
+            r.language    = self.format(x.get("iso_language_code"))
+            r.tweet_id    = self.format(x.get("id"))
+            results.append(r)
+        return results
+
+    def trends(self, **kwargs):
+        """ Returns a list with 10 trending topics on Twitter.
+        """
+        url = URL("https://api.twitter.com/1/trends/1.json")
+        kwargs.setdefault("cached", False)
+        kwargs.setdefault("unicode", True)
+        kwargs.setdefault("throttle", self.throttle)
+        data = url.download(**kwargs)
+        data = json.loads(data)
+        return [u(x.get("name")) for x in data[0].get("trends", [])]
