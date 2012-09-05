39f7e2e3e8cb389e4285aaadda24efd3ea382ba3
Kapil Tundwal <kapil@cruxly.com>
2012-09-04 18:42:01 -0600

    bug fix: daily stat serialization for email. calculate % right.

--- intent/apps/query/models.py
+++ intent/apps/query/models.py
@@ -160,15 +160,15 @@ class DailyStat(models.Model):
 
     def display(self):
         response = 'Daily stat: %s\n' % self.stat_for.strftime('%h %d %Y')
-        response += 'Query: %s\n' & self.stat_of.query.query
+        response += 'Query: %s\n' % self.stat_of.query.query
         response += 'Total tweets processed today: %d\n' % self.document_count
-        response += 'Buy: %d%%\n' % self.buy_count * 100 / self.document_count
-        response += 'Recommendation: %d%%\n' % self.recommendation_count * 100 / self.document_count
-        response += 'Question: %d%%\n' % self.question_count * 100 / self.document_count
-        response += 'Commitment: %d%%\n' % self.commitment_count * 100 / self.document_count
-        response += 'Like: %d%%\n' % self.like_count * 100 / self.document_count
-        response += 'Try: %d%%\n' % self.dislike_count * 100 / self.document_count
-        response += 'Dislike: %d%%\n' % self.try_count * 100 / self.document_count
+        response += 'Buy: %d%%\n' % (self.buy_count * 100 / self.document_count)
+        response += 'Recommendation: %d%%\n' % (self.recommendation_count * 100 / self.document_count)
+        response += 'Question: %d%%\n' % (self.question_count * 100 / self.document_count)
+        response += 'Commitment: %d%%\n' % (self.commitment_count * 100 / self.document_count)
+        response += 'Like: %d%%\n' % (self.like_count * 100 / self.document_count)
+        response += 'Try: %d%%\n' % (self.dislike_count * 100 / self.document_count)
+        response += 'Dislike: %d%%\n' % (self.try_count * 100 / self.document_count)
 
         return response
 
